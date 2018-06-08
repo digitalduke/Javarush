ba80231c4e135a88c6c173fd5be7843e1c8ee494
George Pirogov <i1557@yandex.ru>
2018-06-08 20:58:57 +0500

    Исправление бага ложно положительных срабатываний

--- task2027.java
+++ task2027.java
@@ -6,33 +6,37 @@ import java.util.List;
 
 /*
 Кроссворд
+
+1. Дан двумерный массив, который содержит буквы английского алфавита в нижнем регистре.
+2. Метод detectAllWords должен найти все слова из words в массиве crossword.
+3. Элемент(startX, startY) должен соответствовать первой букве слова, элемент(endX, endY) - последней.
+text - это само слово, располагается между начальным и конечным элементами
+4. Все слова есть в массиве.
+5. Слова могут быть расположены горизонтально, вертикально и по диагонали как в нормальном, так и в обратном порядке.
+
 */
+
 public class Solution {
     public static void main(String[] args) {
         int[][] crossword = new int[][]{
-                {'f', 'd', 'e', 'r', 'l', 'k'},
+                {'f', 'd', 'e', 'r', 'l', 'h'},
                 {'u', 's', 'a', 'm', 'e', 'o'},
                 {'l', 'n', 'g', 'r', 'o', 'v'},
                 {'m', 'l', 'p', 'r', 'r', 'h'},
-                {'e', 'm', 'a', 's', 'j', 'j'}
+                {'a', 'm', 'a', 'h', 'o', 'm'}
         };
-        //detectAllWords(crossword, "home", "same");
-        List<Word> words = detectAllWords(crossword, "fde", "same", "j", "home");
+
+        List<Word> words = detectAllWords(crossword, "home", "same");
+
         for (Word word: words) {
             System.out.println(word);
         }
-        /*
-Ожидаемый результат
-home - (5, 3) - (2, 0)
-same - (1, 1) - (4, 1)
-         */
     }
 
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
         List<Word> result = new ArrayList<>();
         AbstractMap.SimpleEntry<Integer, Integer> endPoint;
-        int[] vectorX = {1, -1, 0, 0, 1, 1, -1, -1};
-        int[] vectorY = {0, 0, 1, -1, 1, -1, -1, 1};
+        int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
 
         // проход по каждому символу матрицы
         for (int y = 0; y < crossword.length; y++) {
@@ -40,12 +44,12 @@ same - (1, 1) - (4, 1)
 
                 // для каждого искомого слова
                 for (String word: words) {
-                    // первая буква совпадает - начинаем поиск по векторам
+                    // если первая буква совпадает - продолжаем поиск по векторам
                     if (crossword[y][x] == word.charAt(0)) {
                         if (word.length() > 1) {
                             // поиск по векторам
                             for (int vector = 0; vector < 8; vector++) {
-                                endPoint = findEnd(crossword, word, x, y, vectorX[vector], vectorY[vector]);
+                                endPoint = findEnd(crossword, word, x, y, direction[vector][0], direction[vector][1]);
                                 if (endPoint != null) {
                                     Word foundedWord = new Word(word);
                                     foundedWord.setStartPoint(x, y);
@@ -55,6 +59,7 @@ same - (1, 1) - (4, 1)
                             }
                         } else {
                             // слово однобуквенное - странно, ну и ладно
+                            // данное условие не обязательно для валидатора javarush
                             Word foundedWord = new Word(word);
                             foundedWord.setStartPoint(x, y);
                             foundedWord.setEndPoint(x, y);
@@ -66,34 +71,35 @@ same - (1, 1) - (4, 1)
             }
         }
 
-
         return result;
     }
 
     private static AbstractMap.SimpleEntry<Integer, Integer> findEnd(int[][] crossword, String word, int startX, int startY, int incX, int incY) {
         int endX = startX, endY = startY;
-        boolean wordFounded = false;
+        boolean wordFounded = true;
 
         try {
             for (int l = 1; l < word.length(); l++) {
+                // прибавляем вектор
                 endX += incX;
                 endY += incY;
-                wordFounded = crossword[endY][endX] == word.charAt(l);
+                // если следующая буква не совпадает, значит не то слово
+                if (crossword[endY][endX] != word.charAt(l)) {
+                    wordFounded = false;
+                    break; // нет смысла продолжать
+                }
             }
         } catch (Exception ignored) {
-            // вышли за границы матрицы - ну и хрен с ним
+            // вышли за границы матрицы - значит точно ничего не нашли
+            wordFounded = false;
         }
 
         if (wordFounded) {
-            //System.out.println("найдено вхождение: " + word + " startX " + startX
-            //        + " startY " + startY
-            //        + " endX " + endX
-            //        + " endY " + endY);
             AbstractMap.SimpleEntry<Integer, Integer> endPoint = new AbstractMap.SimpleEntry<>(endX, endY);
             return endPoint;
         }
 
-    return null;
+        return null;
 
     }
 
