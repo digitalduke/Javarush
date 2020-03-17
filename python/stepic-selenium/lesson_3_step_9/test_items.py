e6afbe7fa6d6e8076636480b99ea87945c0e9439
digitalduke <digitalduke@gmail.com>
2020-03-17 05:05:46 +0500

    move courses and lessons into separate folder

--- python/stepic-selenium/lesson_3_step_9/test_items.py
+++
@@ -1,4 +0,0 @@
-def test_add_to_basket_button_is_present(browser):
-    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    add_to_basket_button = browser.find_element_by_css_selector("#add_to_basket_form > button") 
-    assert add_to_basket_button is not None, 'Add to basket button not found'
